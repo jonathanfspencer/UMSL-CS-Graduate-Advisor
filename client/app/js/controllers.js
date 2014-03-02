ff1fad822d9e6a1767368bc734af0cfc660f6140
Justin Gorinsky <justin@gorinsky.net>
2014-03-02 05:48:14 +0000

    Attempted to add some clarity to the class chooser. Also converted CSS to SASS.

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -6,8 +6,9 @@
 			$scope.classes = [
 				{name: 'CS 1435', status: 'N'},
 				{name: 'CS 1234', status: 'N'},
+				{name: 'CS 2341', status: 'N'},
 				{name: 'CS 2342', status: 'N'},
-				{name: 'CS 2342', status: 'N'}
+				{name: 'CS 2343', status: 'N'}
 			];
 		}]);
 }());
