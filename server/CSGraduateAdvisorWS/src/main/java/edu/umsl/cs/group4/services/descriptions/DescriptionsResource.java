30846bde0891e8ff0016eb5060553191da466f7e
jonathanfspencer <jonathanfspencer@gmail.com>
2014-03-02 13:19:37 -0600

    added response header

--- server/CSGraduateAdvisorWS/src/main/java/edu/umsl/cs/group4/services/descriptions/DescriptionsResource.java
+++ server/CSGraduateAdvisorWS/src/main/java/edu/umsl/cs/group4/services/descriptions/DescriptionsResource.java
@@ -1,11 +1,10 @@
 package edu.umsl.cs.group4.services.descriptions;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -21,19 +20,14 @@ import edu.umsl.cs.group4.services.descriptions.service.DescriptionsServiceURLIm
 @Path("descriptions")
 public class DescriptionsResource {
 	
-
-	@Context
-	private HttpServletResponse response;
-	
 	private DescriptionsService service = new DescriptionsServiceURLImpl();
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Descriptions getDescriptions() throws JAXBException {
-    	response.addHeader("Access-Control-Allow-Origin","*");
+    public Response getDescriptions() throws JAXBException {
     	JAXBContext context = JAXBContext.newInstance(Descriptions.class);
     	Unmarshaller um = context.createUnmarshaller();
         Descriptions descriptions = (Descriptions) um.unmarshal(service.getDescriptionsSource());
-    	return descriptions;
+        return Response.ok(descriptions).header("Access-Control-Allow-Origin","*").build();
     }
 }
