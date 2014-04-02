13fbbd2767cc1328199d9dedb20746f7f8fcc4ef
jonathanfspencer <jonathanfspencer@gmail.com>
2014-04-02 15:33:46 -0500

    more jetties

--- server/CSGraduateAdvisorWS/src/main/java/edu/umsl/cs/group4/server/JettyServer.java
+++ server/CSGraduateAdvisorWS/src/main/java/edu/umsl/cs/group4/server/JettyServer.java
@@ -1,20 +1,25 @@
 package edu.umsl.cs.group4.server;
 
+import java.net.URL;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class JettyServer {
-	
+	private static final String WEBAPPDIR = "/src/main/webapp/";
 	public static void main(String[] args) throws Exception {
 		Server server = new Server(8081);
+		
+		final URL warUrl = JettyServer.class.getClassLoader().getResource(WEBAPPDIR);
+		final String warUrlString = warUrl.toExternalForm();
 		
 		org.eclipse.jetty.webapp.Configuration.ClassList classlist = org.eclipse.jetty.webapp.Configuration.ClassList.setServerDefault(server);
         classlist.addAfter("org.eclipse.jetty.webapp.FragmentConfiguration", "org.eclipse.jetty.plus.webapp.EnvConfiguration", "org.eclipse.jetty.plus.webapp.PlusConfiguration");
  
 		
 		WebAppContext ctx = new WebAppContext();
-		ctx.setDescriptor("src/main/webapp/WEB-INF/web.xml");
-		ctx.setResourceBase("src/main/webapp/");
+		ctx.setDescriptor(warUrlString + "/WEB-INF/web.xml");
+		ctx.setResourceBase(warUrlString);
 		ctx.setContextPath("/");
 		ctx.setParentLoaderPriority(true);
 		server.setHandler(ctx);	 
