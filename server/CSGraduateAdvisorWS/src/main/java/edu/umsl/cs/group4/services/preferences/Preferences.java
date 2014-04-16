4c2fc05467350029dcc092fb222046eb6c377102
jonathanfspencer <jonathanfspencer@gmail.com>
2014-04-15 21:36:04 -0500

    beginnings of scheduling algorithm

--- server/CSGraduateAdvisorWS/src/main/java/edu/umsl/cs/group4/services/preferences/Preferences.java
+++ server/CSGraduateAdvisorWS/src/main/java/edu/umsl/cs/group4/services/preferences/Preferences.java
@@ -1,5 +1,8 @@
 package edu.umsl.cs.group4.services.preferences;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -9,10 +12,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import edu.umsl.cs.group4.services.courses.Course;
+import edu.umsl.cs.group4.services.preferences.ScheduleFacade.CoursesBySession;
+import edu.umsl.cs.group4.services.preferences.ScheduleFacade.CoursesByYear;
 
 @Path("preferences")
 public class Preferences {
 
+	private static final String FALL_SESSION_NAME = "Fall";
+	private static final String SUMMER_SESSION_NAME = "Summer";
+	private static final String SPRING_SESSION_NAME = "Spring";
 	private Integer maxClassesPerSemester;
 	private Integer minClassesPerSemester;
 	private Boolean canTakeDayClasses;
@@ -23,6 +31,7 @@ public class Preferences {
 	private Integer numberOf6000HoursScheduled;
 	private Integer numberOf5000HoursScheduled;
 	private Integer numberOf4000HoursScheduled;
+	private List<String> coreCoursesRemaining;
 	private List<Course> courses;
 	
 	@Path("/apply")
@@ -34,19 +43,66 @@ public class Preferences {
 		//If requirements have been met, exit
 		//Else,
 		//TODO the big nasty algorithm
-//		Requirements requirements = new Requirements();
-		//Loop through courses to 
-			//Separate courses by year and session
+		
+		//create a ScheduleFacade to represent the schedule
+		ScheduleFacade schedule = new ScheduleFacade(preferences.getCourses());
+		//Get an ordered list of the years
+		List<String> years = new ArrayList<String>();
+		Iterator<String> yearIterator = schedule.getCoursesByYear().keySet().iterator();
+		while(yearIterator.hasNext()){
+			years.add(yearIterator.next());
+		}
+		Collections.sort(years);
 		//For each year
+		for(String year : years){
+			CoursesByYear coursesByYear = schedule.getCoursesByYear().get(year);
+			
 			//For each Spring
-				//Determine how many units are scheduled
-				//If room and needed, schedule core courses
-				//If room and needed, schedule a 6000 course
-				//If room and needed, schedule 5000 courses
-				//If room and needed, schedule 4000 courses
+			CoursesBySession springSessionCourses = coursesByYear.getCoursesBySession().get(SPRING_SESSION_NAME);
+			sessionScheduler(year, springSessionCourses, SPRING_SESSION_NAME);
+			
 			//For each Summer
+			CoursesBySession summerSessionCourses = coursesByYear.getCoursesBySession().get(SUMMER_SESSION_NAME);
+			sessionScheduler(year, summerSessionCourses, SUMMER_SESSION_NAME);
+			
 			//For each Fall
+			CoursesBySession fallSessionCourses = coursesByYear.getCoursesBySession().get(FALL_SESSION_NAME);
+			sessionScheduler(year, fallSessionCourses, FALL_SESSION_NAME);
+			
+		}
 		return preferences;
+	}
+
+	private void sessionScheduler(String year, CoursesBySession sessionCourses, String sessionName) {
+		//Determine how many units are scheduled this semester
+		int sessionHours = 0; //TODO use this in the conditionals below
+		for(Course course : sessionCourses.getCourses()){
+			if (course.getScheduledOffering() != null){
+				if(course.getScheduledOffering().getYear() == year && course.getScheduledOffering().getSession().equalsIgnoreCase(sessionName)){
+					sessionHours += Integer.valueOf(course.getCredits());
+				}
+			}
+		}
+		//If room and needed, schedule core courses
+		if(numberOfHoursRemaining > 0 && !coreCoursesRemaining.isEmpty()) {
+			//TODO try to schedule some core courses
+			//if successful, decrement numberOfHoursRemaining and increment corresponding hour counters for 4000 or 5000 level classes
+		}
+		//If room and needed, schedule a 6000 course
+		if(numberOfHoursRemaining > 0 && numberOf6000HoursScheduled < 3) {
+			//TODO try to schedule a 6000 level course
+			//if successful, decrement numberOfHoursRemaining and increment numberOf6000HoursScheduled 
+		}
+		//If room and needed, schedule 5000 courses
+		if(numberOfHoursRemaining > 0 && numberOf5000HoursScheduled < 15) {
+			//TODO try to schedule some 5000 level classes
+			//if successful, decrement numberOfHoursRemaining and increment numberOf5000HoursScheduled
+		}
+		//If room and needed, schedule 4000 courses
+		if(numberOfHoursRemaining > 0 && numberOf4000HoursScheduled < 12) {
+			//TODO try to schedule some 4000 level classes
+			//if successful, decrement numberOfHoursRemaining and increment numberOf4000HoursScheduled
+		}
 	}
 
 	public Integer getMaxClassesPerSemester() {
@@ -135,5 +191,13 @@ public class Preferences {
 
 	public void setNumberOf4000HoursScheduled(Integer numberOf4000HoursScheduled) {
 		this.numberOf4000HoursScheduled = numberOf4000HoursScheduled;
+	}
+
+	public List<String> getCoreCoursesRemaining() {
+		return coreCoursesRemaining;
+	}
+
+	public void setCoreCoursesRemaining(List<String> coreCoursesRemaining) {
+		this.coreCoursesRemaining = coreCoursesRemaining;
 	}
 }
---
+++ server/CSGraduateAdvisorWS/src/main/java/edu/umsl/cs/group4/services/preferences/ScheduleFacade.java
@@ -0,0 +1,79 @@
+package edu.umsl.cs.group4.services.preferences;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import edu.umsl.cs.group4.services.courses.Course;
+import edu.umsl.cs.group4.services.courses.Course.Offering;
+
+/**
+ * A facade to represent a student's schedule by year and session.  The key
+ * for the coursesByYear map is the year.  The key for the coursesBySession
+ * map is the session.
+ * 
+ * @author jonathan
+ *
+ */
+public class ScheduleFacade {
+
+	private Map<String,CoursesByYear> coursesByYear;
+	
+	public class CoursesByYear {
+		private Map<String,CoursesBySession> coursesBySession;
+		
+
+		public Map<String,CoursesBySession> getCoursesBySession() {
+			return coursesBySession;
+		}
+
+		public void setCoursesBySession(Map<String,CoursesBySession> coursesBySession) {
+			this.coursesBySession = coursesBySession;
+		}
+	}
+	
+	public class CoursesBySession {
+		private List<Course> courses;
+
+		public List<Course> getCourses() {
+			return courses;
+		}
+
+		public void setCourses(List<Course> courses) {
+			this.courses = courses;
+		}
+	}
+	
+
+	/**
+	 * Constructor to build a ScheduleFacade out of a List of Course
+	 * @param courses
+	 */
+	public ScheduleFacade(List<Course> courses) {
+		this.coursesByYear = new HashMap<String,CoursesByYear>();
+		for(Course course: courses){
+			for(Offering offering : course.getOfferings()){
+				//see if we need to add a year
+				if(!this.coursesByYear.containsKey(offering.getYear())){
+					//add the new year
+					this.coursesByYear.put(offering.getYear(), new CoursesByYear());
+				} 
+				//see if we need to add a session
+				if((!this.coursesByYear.get(offering.getYear()).getCoursesBySession().containsKey(offering.getSession()))){
+					//add the new session
+					this.coursesByYear.get(offering.getYear()).getCoursesBySession().put(offering.getSession(), new CoursesBySession());
+				}
+				//add the course to the session
+				this.coursesByYear.get(offering.getYear()).getCoursesBySession().get(offering.getSession()).getCourses().add(course);
+			}
+		}
+	}
+
+	public Map<String,CoursesByYear> getCoursesByYear() {
+		return coursesByYear;
+	}
+
+	public void setCoursesByYear(Map<String,CoursesByYear> coursesByYear) {
+		this.coursesByYear = coursesByYear;
+	}
+}
